8d0ced319c62507c012b60b903763d521fe9e137
gr1zz-sharp <keithinscore@gmail.com>
2022-02-16 10:50:44 -0600

    initialize

---
+++ src/main/java/com/example/finaltasktracker/FinalTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.finaltasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finaltasktracker/FinalTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finaltasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
