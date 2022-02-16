49e90e737ed7f8728615c86987eccb7abafa05d8
gr1zz-sharp <keithinscore@gmail.com>
2022-02-16 13:13:35 -0600

    add endpoints and connect to database

--- src/main/java/com/example/finaltasktracker/Repository/TaskRepository.java
+++ src/main/java/com/example/finaltasktracker/Repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package com.example.finaltasktracker.Repository;
 
-public interface TaskRepository {
+import com.example.finaltasktracker.Model.Tasks;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Tasks, Integer>, JpaSpecificationExecutor<Tasks> {
 }
--- src/main/java/com/example/finaltasktracker/Service/TaskService.java
+++ src/main/java/com/example/finaltasktracker/Service/TaskService.java
@@ -1,4 +1,24 @@
 package com.example.finaltasktracker.Service;
 
+import com.example.finaltasktracker.Model.Tasks;
+import com.example.finaltasktracker.Repository.TaskRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class TaskService {
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Tasks> getTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Tasks saveTask(Tasks tasks) {
+        return taskRepository.save(tasks);
+    }
 }
